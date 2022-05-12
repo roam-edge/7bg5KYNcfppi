3a16a601f9e7691eb43470313825052c7f6504d8
roam-edge <3336851348@qq.com>
2022-05-12 21:13:09 +0800

    :tada:

---
+++ assets/index.84eb3bdb.js
@@ -0,0 +1,1 @@
+import{Y as t,Z as s,ay as a}from"./index.dd32f82b.js";const u={setup(){const e=t(),r=s();e.replace(r.fullPath.replace(/^\/redirect/,""))},render(){return a("div")}};export{u as default};

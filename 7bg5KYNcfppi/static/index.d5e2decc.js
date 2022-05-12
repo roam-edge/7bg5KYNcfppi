389ecd7255dbafff93495e455b5ef2a714e1209a
roam-edge <3336851348@qq.com>
2022-05-12 21:20:51 +0800

    :tada:

---
+++ 7bg5KYNcfppi/static/index.d5e2decc.js
@@ -0,0 +1,1 @@
+import{Y as t,Z as s,ay as a}from"./index.7bad512b.js";const u={setup(){const e=t(),r=s();e.replace(r.fullPath.replace(/^\/redirect/,""))},render(){return a("div")}};export{u as default};

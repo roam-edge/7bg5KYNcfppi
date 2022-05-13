75c6fcc2490fb9f80417ad86a3c7377f81703f20
roam-edge <3336851348@qq.com>
2022-05-13 18:34:54 +0800

    :tada:

---
+++ assets/index.c2ab3158.js
@@ -0,0 +1,2 @@
+import{Y as t,Z as s,ay as a}from"./index.953a039b.js";const u={setup(){const e=t(),r=s();e.replace(r.fullPath.replace(/^\/redirect/,""))},render(){return a("div")}};export{u as default};
+//# sourceMappingURL=index.c2ab3158.js.map
--- assets/index.c89cf9e4.js
+++
@@ -1,1 +0,0 @@
-import{Y as t,Z as s,ay as a}from"./index.7ca58859.js";const u={setup(){const e=t(),r=s();e.replace(r.fullPath.replace(/^\/redirect/,""))},render(){return a("div")}};export{u as default};
